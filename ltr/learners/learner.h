4b3dd47c2bac14a45215a027f59495beb54dad4c
Lev Tolmachev <tolmalev@yandex-team.ru>
2011-12-27 14:54:55 +0400

    added BaseLearner

--- ltr/learners/learner.h
+++ ltr/learners/learner.h
@@ -10,15 +10,16 @@
 #include "ltr/interfaces/aliaser.h"
 #include "ltr/interfaces/parameterized.h"
 #include "ltr/data/data_set.h"
+#include "ltr/scorers/scorer.h"
 #include "ltr/feature_converters/feature_converter.h"
 #include "ltr/parameters_container/parameters_container.h"
 
 namespace ltr {
 
-template< class TElement, class TScorer >
-class Learner : public Reporter, public Aliaser, public Parameterized {
+template< class TElement >
+class BaseLearner : public Reporter, public Aliaser, public Parameterized {
   public:
-  typedef boost::shared_ptr<Learner> Ptr;
+  typedef boost::shared_ptr<BaseLearner> Ptr;
 
   void learn(const DataSet<TElement>& data);
 
@@ -26,8 +27,7 @@ class Learner : public Reporter, public Aliaser, public Parameterized {
       typename ltr::FeatureConverter::ConstPtr p_FeatureConverter);
 
   virtual void reset() = 0;
-  virtual void setInitialScorer(const TScorer& in_scorer) = 0;
-  virtual TScorer make() const = 0;
+  virtual Scorer::Ptr makeScorerPtr() const = 0;
 
   const FeatureConverterArray& getFeatureConverters() const {
     return featureConverters_;
@@ -36,7 +36,7 @@ class Learner : public Reporter, public Aliaser, public Parameterized {
     this->featureConverters_ = featureConverters;
   }
 
-  virtual ~Learner() {}
+  virtual ~BaseLearner() {}
 
   private:
   virtual void learnImpl(const DataSet<TElement>& data) = 0;
@@ -44,14 +44,29 @@ class Learner : public Reporter, public Aliaser, public Parameterized {
   FeatureConverterArray featureConverters_;
 };
 
+
 template< class TElement, class TScorer >
-void Learner< TElement, TScorer >::addFeatureConverter(
+class Learner : public BaseLearner< TElement > {
+  public:
+  virtual TScorer make() const = 0;
+  virtual Scorer::Ptr makeScorerPtr() const;
+  virtual void setInitialScorer(const TScorer& in_scorer) = 0;
+};
+
+template< class TElement, class TScorer >
+Scorer::Ptr Learner< TElement, TScorer >::makeScorerPtr() const {
+  Scorer *scr = new TScorer(make());
+  return Scorer::Ptr(scr);
+}
+
+template< class TElement >
+void BaseLearner< TElement >::addFeatureConverter(
     typename ltr::FeatureConverter::ConstPtr p_FeatureConverter) {
   featureConverters_.push_back(p_FeatureConverter);
 }
 
-template< class TElement, class TScorer >
-void Learner< TElement, TScorer >::learn(const DataSet<TElement>& data) {
+template< class TElement >
+void BaseLearner< TElement >::learn(const DataSet<TElement>& data) {
   DataSet<TElement> sourceData = data.deepCopy();
   DataSet<TElement> convertedData;
   for (size_t featureConverterIdx = 0;
