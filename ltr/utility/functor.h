a45c5ff89077f749968bdf7a3659e2b61581747b
Lev Tolmachev <tolmalev@yandex-team.ru>
2012-01-31 23:50:32 +0400

    doxygen for functor.h

--- ltr/utility/functor.h
+++ ltr/utility/functor.h
@@ -18,6 +18,10 @@ namespace ltr {
 
 extern size_t n_serializable_functors_;
 
+/**
+ * Base class for all serializable functors
+ * TValue - type of the result of the function
+ */
 template<class TValue>
 class SerializableFunctor {
   private:
@@ -29,19 +33,41 @@ class SerializableFunctor {
       id = n_serializable_functors_++;
     }
 
+    /**
+     * Function returns unique Id of the functor
+     */
     int getId() {return id;}
+
+    /**
+     * Function returns default function name for this functor
+     */
     string getDefaultFunctionName() {
       return "function" + lexical_cast<string>(id);
     }
 
+    /**
+     * Function returns serialized code of this functor
+     * @param function_name - name of the function to generate
+     */
     virtual string generateCppCode(const string& function_name) const = 0;
+
+    /**
+     * Function returns serialized code of this functor
+     * with default function name
+     */
     string generateCppCode() {
       return generateCppCode(getDefaultFunctionName());
     }
 
+    /**
+     * Function returns the value of this functor for the given object
+     */
     TValue operator()(const Object& obj) const {
       return value(obj);
     }
+    /**
+     * Function returns the value of this functor for the given object
+     */
     virtual TValue value(const Object& obj) const = 0;
     virtual ~SerializableFunctor() {}
 };
