f2bf002a14cc60d559e148b2818f3bc2028d523a
Marat Baydasov <baydasov@yandex-team.ru>
2012-02-05 21:25:47 +0400

    crossvalidation 1st unittest works now

--- ltr/crossvalidation/splitter_unittest.h
+++ ltr/crossvalidation/splitter_unittest.h
@@ -7,19 +7,24 @@
 
 #include "ltr/data/data_set.h"
 #include "ltr/data/object.h"
+#include "ltr/data/feature_info.h"
 
 using ltr::DataSet;
 using ltr::Object;
+using ltr::FeatureInfo;
+
+const int data_lenght = 11;
 
 class SplitterTest : public ::testing::Test {
   protected:
   virtual void SetUp() {
-    int data_lenght = 11;
     for (int i = 0; i < data_lenght; ++i) {
       Object obj;
       obj << i;
       data.add(obj);
     }
+    // note that this dataset has invalid feature info
+    // but it's not significant for splitters
   }
 
   protected:
