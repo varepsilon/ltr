59f8d94f2a7d16e221643238acd65bb157fe8797
Marat Baydasov <baydasov@yandex-team.ru>
2012-02-06 22:56:13 +0400

    + style

--- ltr/measures/average_precision.cc
+++ ltr/measures/average_precision.cc
@@ -1,10 +1,10 @@
 // Copyright 2011 Yandex
 
-#include "ltr/measures/average_precision.h"
-
 #include <string>
 #include <algorithm>
 #include <boost/lexical_cast.hpp>
+
+#include "ltr/measures/average_precision.h"
 #include "ltr/measures/utils/measure_utility.h"
 
 using std::sort;
