8b40b8228a022cfc9da5cc63b98dca6859a61721
Marat Baydasov <baydasov@yandex-team.ru>
2012-02-14 13:50:27 +0400

    uncomment bad feature converters unittest

--- ltr/feature_converters/feature_converters_unittest.cc
+++ ltr/feature_converters/feature_converters_unittest.cc
@@ -128,8 +128,7 @@ TEST_F(FeatureConvertersTest, TestingFeatureNormalisationNoFailureObject) {
       ltr::utility::MarkDataSet(learn_data_pointwise, learner.make()));
 };
 
-// waiting for unittest for AveragePrecision
-/*
+
 TEST_F(FeatureConvertersTest, TestingFeatureNormalisationNoFailureObjectList) {
   ltr::FeatureNormalizerLearner<ltr::ObjectList> normalizerLearner;
   EXPECT_NO_THROW(normalizerLearner.learn(learn_data_listwise));
@@ -143,4 +142,3 @@ TEST_F(FeatureConvertersTest, TestingFeatureNormalisationNoFailureObjectList) {
   EXPECT_NO_THROW(
       ltr::utility::MarkDataSet(learn_data_listwise, learner.make()));
 };
-*/
