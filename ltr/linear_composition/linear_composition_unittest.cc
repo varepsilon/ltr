d6ca3176088be68025c509c9b8529f3d30bc1964
Marat Baydasov <baydasov@yandex-team.ru>
2012-02-27 15:37:04 +0400

    + simple begging and RSM unittests

--- ltr/linear_composition/linear_composition_unittest.cc
+++ ltr/linear_composition/linear_composition_unittest.cc
@@ -18,6 +18,8 @@
 
 #include "ltr/learners/best_feature_learner.h"
 #include "ltr/measures/abs_error.h"
+#include "ltr/data_preprocessors/begging_preprocessor_learner.h"
+#include "ltr/feature_converters/RSM_feature_converter_learner.h"
 
 using ltr::Object;
 using ltr::DataSet;
@@ -26,6 +28,8 @@ using ltr::LinearCompositionScorer;
 using ltr::utility::MarkDataSet;
 using ltr::utility::DoubleEqual;
 
+using ltr::RSMFeatureConverterLearner;
+using ltr::BeggingPreprocessorLearner;
 using ltr::BestFeatureLearner;
 using ltr::lc::LinearCompositionLearner;
 using ltr::AbsError;
@@ -36,12 +40,12 @@ const int data_size = 11;
 class LinearCompositionTest : public ::testing::Test {
   public:
   LinearCompositionTest()
-    : data(FeatureInfo(2)) {}
+    : data(FeatureInfo(4)) {}
   protected:
   virtual void SetUp() {
     for (int i = 0; i < data_size; ++i) {
       Object obj;
-      obj << i << data_size - i << i * i;
+      obj << i << data_size - i << i * i << i * (data_size - i);
       obj.setActualLabel(i);
       obj.setPredictedLabel(data_size - i);
       data.add(obj);
@@ -78,4 +82,60 @@ TEST_F(LinearCompositionTest, SimpleLinearCompositionTest) {
   LinearCompositionScorer av_lin_scorer = av_lc_learner.make();
   MarkDataSet(data, av_lin_scorer);
   EXPECT_NEAR(0.0, abs_error->average(data), 1e-8);
+}
+
+TEST_F(LinearCompositionTest, BeggingSimpleLinearCompositionTest) {
+  LinearCompositionLearner<Object> lc_learner;
+
+  AbsError::Ptr abs_error(new AbsError);
+  lc_learner.setMeasure(abs_error);
+
+  BeggingPreprocessorLearner<Object>::Ptr
+    begging(new BeggingPreprocessorLearner<Object>);
+  lc_learner.setDataPreprocessorLearner(begging);
+
+  BestFeatureLearner<Object>::Ptr bf_learner(new BestFeatureLearner<Object>);
+  lc_learner.setWeakLearner(bf_learner);
+
+  lc_learner.learn(data);
+  LinearCompositionScorer lin_scorer = lc_learner.make();
+
+  EXPECT_EQ(10, lin_scorer.size());
+  for (int i = 0; i < lin_scorer.size(); ++i) {
+    MarkDataSet(data, *lin_scorer[i].scorer);
+    EXPECT_TRUE(DoubleEqual(0.0, abs_error->average(data)))
+      << abs_error->average(data);
+  }
+
+  begging->setDoubleParameter("SELECTED_PART", 2);
+  lc_learner.setDataPreprocessorLearner(begging);
+  lc_learner.setIntParameter("NUMBER_OF_ITERATIONS", 15);
+  lc_learner.learn(data);
+  lin_scorer = lc_learner.make();
+
+  EXPECT_EQ(25, lin_scorer.size());
+  for (int i = 0; i < lin_scorer.size(); ++i) {
+    MarkDataSet(data, *lin_scorer[i].scorer);
+    EXPECT_TRUE(DoubleEqual(0.0, abs_error->average(data)))
+      << abs_error->average(data);
+  }
+}
+
+TEST_F(LinearCompositionTest, RSMSimpleLinearCompositionTest) {
+  LinearCompositionLearner<Object> lc_learner;
+
+  AbsError::Ptr abs_error(new AbsError);
+  lc_learner.setMeasure(abs_error);
+
+  RSMFeatureConverterLearner<Object>::Ptr
+    begging(new RSMFeatureConverterLearner<Object>);
+  lc_learner.setFeatureConverterLearner(begging);
+
+  BestFeatureLearner<Object>::Ptr bf_learner(new BestFeatureLearner<Object>);
+  lc_learner.setWeakLearner(bf_learner);
+
+  lc_learner.learn(data);
+  LinearCompositionScorer lin_scorer = lc_learner.make();
+
+  EXPECT_NO_THROW(MarkDataSet(data, lin_scorer));
 }
