a37c58fab1fb8a5f745bd2854ed40c1ffb14a379
Lev Tolmachev <tolmalev@yandex-team.ru>
2012-03-06 23:31:43 +0400

    added normalized measure and it's unittest

---
+++ ltr/measures/normalized_measure.h
@@ -0,0 +1,103 @@
+// Copyright 2012 Yandex
+
+#ifndef LTR_MEASURES_NORMALIZED_MEASURE_H_
+#define LTR_MEASURES_NORMALIZED_MEASURE_H_
+
+#include "ltr/utility/numerical.h"
+#include "ltr/measures/measure.h"
+
+namespace ltr {
+/**
+ * Normalized measure. Can be used for any approach.
+ * Returns lineal normalisation of the given weak measure.
+ * Best and worst values of the rasulting measure are given in parameters.
+ * Can't normalize infinity measure.
+ */
+template<class TElement>
+class NormalizedMeasure : public Measure<TElement> {
+  private:
+    typename Measure<TElement>::Ptr weak_measure_;
+
+    virtual double get_measure(const TElement& element) const {
+      if (weak_measure_ == NULL)
+        throw std::logic_error("No weak measure");
+      if (weak_measure_->best() == utility::Inf ||
+          weak_measure_->best() == -utility::Inf ||
+          weak_measure_->worst() == utility::Inf ||
+          weak_measure_->worst() == -utility::Inf) {
+        throw std::logic_error("can't normalize infinity measure");
+      }
+      double best = this->getDoubleParameter("BEST");
+      double worst = this->getDoubleParameter("WORST");
+      return (weak_measure_->value(element) - weak_measure_->worst()) *
+                (best - worst) /
+                  (weak_measure_->best() - weak_measure_->worst())
+              + worst;
+    }
+
+  public:
+    typedef boost::shared_ptr<NormalizedMeasure> Ptr;
+
+    explicit NormalizedMeasure(
+        const ParametersContainer& parameters = ParametersContainer())
+        : Measure<TElement>("Normalised measure") {
+      this->setDefaultParameters();
+      this->copyParameters(parameters);
+    }
+
+    NormalizedMeasure(double worst, double best)
+        : Measure<TElement>("Normalised measure") {
+      this->setDefaultParameters();
+      this->setDoubleParameter("WORST", worst);
+      this->setDoubleParameter("BEST", best);
+      this->checkParameters();
+    }
+
+    explicit NormalizedMeasure(
+        typename Measure<TElement>::Ptr weak_measure,
+        const ParametersContainer& parameters = ParametersContainer())
+        : Measure<TElement>("Normalised measure") {
+      this->setWeakMeasure(weak_measure);
+      this->setDefaultParameters();
+      this->copyParameters(parameters);
+    }
+
+    NormalizedMeasure(
+        typename Measure<TElement>::Ptr weak_measure,
+        double worst, double best) {
+      this->setWeakMeasure(weak_measure);
+      this->setDefaultParameters();
+      this->setDoubleParameter("BEST", best);
+      this->setDoubleParameter("WORST", worst);
+    }
+
+    void setWeakMeasure(typename Measure<TElement>::Ptr weak_measure) {
+      weak_measure_ = weak_measure;
+    }
+
+    template<class TMeasure>
+    void setWeakMeasure(const TMeasure& weak_measure) {
+      weak_measure_ =
+        typename Measure<TElement>::Ptr(new TMeasure(weak_measure));
+    }
+
+    virtual void checkParameters() {
+      if (this->getDoubleParameter("BEST") ==
+          this->getDoubleParameter("WORST"))
+        throw std::logic_error("Best and worst values must be different");
+    }
+
+    virtual void setDefaultParameters() {
+      this->addDoubleParameter("BEST", 1);
+      this->addDoubleParameter("WORST", -1);
+    }
+
+    virtual double worst() const {
+      return this->getDoubleParameter("WORST");
+    }
+    virtual double best() const {
+      return this->getDoubleParameter("BEST");
+    }
+};
+}
+#endif  // LTR_MEASURES_NORMALIZED_MEASURE_H_
