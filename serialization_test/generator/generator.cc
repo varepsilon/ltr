6230180e8b57f1f8dc8a0e2155e78b1507c990a5
Marat Baydasov <baydasov@yandex-team.ru>
2012-04-02 20:36:53 +0400

    link test

--- serialization_test/generator/generator.cc
+++ serialization_test/generator/generator.cc
@@ -1,20 +1,20 @@
 // Copyright 2012 Yandex
 
-// #include <boost/lexical_cast.hpp>
+#include <boost/lexical_cast.hpp>
 
-// #include "serialization_test/generator/generator_utility.h"
+#include "serialization_test/generator/generator_utility.h"
 
-// #include "ltr/measures/measure.h"
-// #include "ltr/learners/best_feature_learner.h"
+#include "ltr/measures/measure.h"
+#include "ltr/learners/best_feature_learner.h"
 // #include "ltr/learners/id3_learner.h"
-// #include "ltr/measures/abs_error.h"
+#include "ltr/measures/abs_error.h"
 
-// using ltr::BestFeatureLearner;
-// using ltr::AbsError;
-// using ltr::Measure;
+using ltr::BestFeatureLearner;
+using ltr::AbsError;
+using ltr::Measure;
 // using ltr::ID3_Learner;
 
-// using namespace serialization_test;
+using serialization_test::Generator;
 
 #include "utility/timer.h"
 #include "utility/logger.h"
@@ -23,12 +23,12 @@ int main(int argc, char* argv[]) {
   logger::LogStream log;
   timer::Timer t;
 
-  // Generator generator;
+  Generator generator;
 
-  // Measure<Object>::Ptr abs_error(new AbsError());
-  // BestFeatureLearner<Object>::Ptr bf_learner(
-  //    new BestFeatureLearner<Object>(abs_error));
-  // bf_learner->learn(generator.train_data);
+  Measure<Object>::Ptr abs_error(new AbsError());
+  BestFeatureLearner<Object>::Ptr bf_learner(
+      new BestFeatureLearner<Object>(abs_error));
+  bf_learner->learn(generator.train_data);
   // generator.setScorerTest(bf_learner, "bf_learner");
 
   // ID3_Learner::Ptr id3_learner(new ID3_Learner);
