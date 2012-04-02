2c21a3a57eb723fe8e07ded4512589390537f112
Marat Baydasov <baydasov@yandex-team.ru>
2012-04-02 22:32:14 +0400

    link test

--- serialization_test/generator/generator.cc
+++ serialization_test/generator/generator.cc
@@ -8,15 +8,15 @@
 #include "ltr/learners/best_feature_learner.h"
 #include "ltr/learners/id3_learner.h"
 #include "ltr/measures/abs_error.h"
-#include "ltr/learners/gp_learner.h"
-#include "ltr/linear_composition/linear_composition_learner.h"
+// #include "ltr/learners/gp_learner.h"
+// #include "ltr/linear_composition/linear_composition_learner.h"
 
 using ltr::BestFeatureLearner;
 using ltr::AbsError;
 using ltr::Measure;
 using ltr::ID3_Learner;
-using ltr::gp::GPLearner;
-using ltr::lc::LinearCompositionLearner;
+// using ltr::gp::GPLearner;
+// using ltr::lc::LinearCompositionLearner;
 
 using serialization_test::Generator;
 
@@ -30,9 +30,9 @@ int main(int argc, char* argv[]) {
   bf_learner->learn(generator.train_data);
   generator.setScorerTest(bf_learner, "bf_learner");
 
-  GPLearner<Object>::Ptr gp_learner(new GPLearner<Object>(abs_error));
-  gp_learner->learn(generator.train_data);
-  generator.setScorerTest(gp_learner, "gp_learner");
+  // GPLearner<Object>::Ptr gp_learner(new GPLearner<Object>(abs_error));
+  // gp_learner->learn(generator.train_data);
+  // generator.setScorerTest(gp_learner, "gp_learner");
 
   ID3_Learner::Ptr id3_learner(new ID3_Learner);
   id3_learner->learn(generator.train_data);
@@ -40,11 +40,11 @@ int main(int argc, char* argv[]) {
 
   // may be a problem with function name conflicts in code generated,
   // when id3 is used as weak learner
-  LinearCompositionLearner<Object>::Ptr simple_lc_learner(
-    new LinearCompositionLearner<Object>);
-  simple_lc_learner->setMeasure(abs_error);
-  simple_lc_learner->setWeakLearner(bf_learner);
-  simple_lc_learner->learn(generator.train_data);
+  // LinearCompositionLearner<Object>::Ptr simple_lc_learner(
+  //  new LinearCompositionLearner<Object>);
+  // simple_lc_learner->setMeasure(abs_error);
+  // simple_lc_learner->setWeakLearner(bf_learner);
+  // simple_lc_learner->learn(generator.train_data);
   // generator.setScorerTest(simple_lc_learner, "simple_lc_learner");
 
   generator.setMainCode();
