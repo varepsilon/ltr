2715196eb8cda69754b5456b01e3bd4773589564
Marat Baydasov <baydasov@yandex-team.ru>
2012-04-08 19:26:50 +0400

    + gcc patch

--- doc/examples/data_set_tutorial/tutorial_data_example3.cpp
+++ doc/examples/data_set_tutorial/tutorial_data_example3.cpp
@@ -4,6 +4,7 @@
 using std::cout;
 using std::vector;
 using std::endl;
+using std::logic_error;
 using ltr::Object;
 
 int main() {
@@ -12,11 +13,11 @@ int main() {
   obj.setMetaInfo("info1", "value1");
   cout << obj.getMetaInfo("info1") << endl;
 
-  try {                                 //
-    obj.getMetaInfo("info2");           // Exception, because Object doesn't have meta info named "info2".
-  } catch(std::logic_error err) {       //
-    cout << err.what() << endl;         //
-  }                                     //
+  try {                               //
+    obj.getMetaInfo("info2");         // Exception, because Object doesn't have meta info named "info2".
+  } catch(logic_error err) {          //
+    cout << err.what() << endl;       //
+  }                                   //
 
   obj.setMetaInfo("info2", "value2");
   cout << obj.getMetaInfo("info2") << endl;
