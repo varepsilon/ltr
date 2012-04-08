81b222b613154a2e8c4e49459755475a592e0d33
Marat Baydasov <baydasov@yandex-team.ru>
2012-04-08 19:00:15 +0400

    + doc cmake for *.out generating

---
+++ doc/sources.cmake
@@ -0,0 +1,8 @@
+SET(Sources ${Doc_Path}/examples/data_set_tutorial/hello_world
+            ${Doc_Path}/examples/data_set_tutorial/tutorial_data_example1
+			${Doc_Path}/examples/data_set_tutorial/tutorial_data_example2
+			${Doc_Path}/examples/data_set_tutorial/tutorial_data_example3
+			${Doc_Path}/examples/data_set_tutorial/tutorial_data_example4
+			${Doc_Path}/examples/data_set_tutorial/tutorial_data_example5
+			${Doc_Path}/examples/data_set_tutorial/tutorial_data_example6
+			${Doc_Path}/examples/data_set_tutorial/tutorial_data_example7)
