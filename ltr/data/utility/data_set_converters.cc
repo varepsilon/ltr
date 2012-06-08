7fc9b04cb2b28976eae40f4292adb0fa2b9d2c50
arbabenko <arbabenko@yandex-team.ru>
2012-06-08 17:37:59 +0400

    pretty converters

--- ltr/data/utility/data_set_converters.cc
+++ ltr/data/utility/data_set_converters.cc
@@ -5,48 +5,53 @@
 namespace ltr {
 namespace utility {
 
-void DefaultConverter::operator()(const ObjectList& list,
-                                vector<ObjectPair>* result) {
+void ListToAllPairsConverter::operator()(const ObjectList& list,
+                                         vector<ObjectPair>* result) {
   result->clear();
-  for (int i = 0; i < list.size(); ++i) {
-    for (int j = 0; j < i; ++j) {
-      result->push_back(ObjectPair(list[i], list[j]));
+  for (int first_index = 0; first_index < (int)list.size(); ++first_index) {
+    for (int second_index = 0; second_index < first_index; ++second_index) {
+      result->push_back(ObjectPair(list[first_index], list[second_index]));
     }
   }
 }
 
 template<>
-DataSet< ObjectPair > convertDataSet(const DataSet<ObjectList>& data,
-    ListToPairConvertionStrategy::Ptr strategy) {
-    DataSet< ObjectPair > result;
-    for (int i = 0; i < data.size(); i++) {
-      vector< ObjectPair > tmp;
-      (*strategy)(data[i], &tmp);
-      for (int j = 0; j < tmp.size(); j++)
-        result << tmp[j];
+DataSet<ObjectPair> convertDataSet(const DataSet<ObjectList>& data,
+                                   ListToPairConvertionStrategy::Ptr strategy) {
+    DataSet<ObjectPair> result;
+    for (int list_index = 0; list_index < (int)data.size(); ++list_index) {
+      vector<ObjectPair> pairs;
+      (*strategy)(data[list_index], &pairs);
+      for (int pair_index = 0; pair_index < (int)pairs.size(); ++pair_index) {
+        result << pairs[pair_index];
+      }
     }
     return result;
 }
 
 template<>
-DataSet< Object > convertDataSet(const DataSet<ObjectList>& data,
-    ListToPairConvertionStrategy::Ptr strategy) {
-  DataSet< Object > result;
-  for (int i = 0; i < data.size(); i++) {
-    for (int j = 0; j < data[i].size(); j++) {
-      result << data[i][j];
+DataSet<Object> convertDataSet(const DataSet<ObjectList>& data,
+                               ListToPairConvertionStrategy::Ptr strategy) {
+  DataSet<Object> result;
+  for (int list_index = 0; list_index < (int)data.size(); ++list_index) {
+    for (int object_index = 0;
+         object_index < data[list_index].size();
+         ++object_index) {
+      result << data[list_index][object_index];
     }
   }
   return result;
 }
 
 template<>
-DataSet< Object > convertDataSet(const DataSet<ObjectPair>& data,
-    ListToPairConvertionStrategy::Ptr strategy) {
-  DataSet< Object > result;
-  for (int i = 0; i < data.size(); i++) {
-    for (int j = 0; j < data[i].size(); j++) {
-      result << data[i][j];
+DataSet<Object> convertDataSet(const DataSet<ObjectPair>& data,
+                               ListToPairConvertionStrategy::Ptr strategy) {
+  DataSet<Object> result;
+  for (int pair_index = 0; pair_index < (int)data.size(); ++pair_index) {
+    for (int object_index = 0;
+         object_index < data[pair_index].size();
+         ++object_index) {
+      result << data[pair_index][object_index];
     }
   }
   return result;
