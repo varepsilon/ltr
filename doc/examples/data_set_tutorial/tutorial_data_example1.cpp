43102e81b99fec9b8aabe3639ccd30cf7d489ac8
Egor Samosvat <sameg@yandex-team.ru>
2012-09-04 22:15:54 +0400

    Update DataSet tutorial examples

--- doc/examples/data_set_tutorial/tutorial_data_example1.cpp
+++ doc/examples/data_set_tutorial/tutorial_data_example1.cpp
@@ -9,21 +9,27 @@ using ltr::Object;
 int main() {
   Object obj;
   obj << 1.1 << 45.3 << 23 << 1e8;        // Object contains now 4 features
+  cout << "obj = " << obj << endl;        // You can print an object
 
-  cout << obj.feature_count() << endl;     // Method featureCount returns count of features in object
+  cout << "obj.feature_count() = " 
+	   << obj.feature_count() << endl;     // Method featureCount returns count of features in object
 
-  obj << 2.4;                             // You can add new features at any time.
-  cout << obj.feature_count()              // Returns 5
-       << endl << endl;
+  obj << 2.4;                              // You can add new features at any time.
+  cout << "feature was added" << endl;
 
-  cout << obj.features()[0] << " "        // Two ways to get the feature value.
-       << obj.features().at(1) << endl;   //
+  cout << "obj.feature_count() = " 
+	   << obj.feature_count() << endl;     // Returns 5
+
+  cout << "obj[0] = "  << obj[0] << ", "          // Two ways to get the feature value.
+       << "obj.at(1) = "  << obj.at(1) << endl;   //
 
   vector<double> new_features;
   new_features.push_back(1);
   new_features.push_back(5.0);
   new_features.push_back(2);
 
-  obj.features() = new_features;          // You can also set all objects features an once.
+  obj.features() = new_features;  // You can also set all objects features at once.
+  cout << "obj = " << obj;
+
   return 0;
 }
--- doc/examples/data_set_tutorial/tutorial_data_example4.cpp
+++ doc/examples/data_set_tutorial/tutorial_data_example4.cpp
@@ -11,18 +11,18 @@ int main() {
 
   obj1 << 2 << 4 << 6 << 8;
 
-  obj2 = obj1;                    // Simple copy
+  obj2 = obj1;                // Simple copy
 
-  cout << obj2.features()[1] << endl;
-  obj1.features()[1] = -1;          // After changing value in obj1
-  cout << obj2.features()[1]        // Value in obj2 changes too
+  cout << obj2[1] << endl;
+  obj1[1] = -1;               // After changing value in obj1
+  cout << obj2[1]             // Value in obj2 changes too
        << endl << endl;
 
   obj2 = obj1.deepCopy();
 
-  cout << obj2.features()[1] << endl;
-  obj1.features()[1] = 5;           // Now nothing changes in obj2
-  cout << obj2.features()[1];       // After changing obj1
+  cout << obj2[1] << endl;
+  obj1[1] = 5;                // Now nothing changes in obj2
+  cout << obj2[1];            // After changing obj1
 
   return 0;
 }
