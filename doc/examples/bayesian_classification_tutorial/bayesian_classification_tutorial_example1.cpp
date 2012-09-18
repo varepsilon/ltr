64ef4433f452884a4de67b6ec6fd2f8daa87377f
Igor Bichurin <rinigan@yandex-team.ru>
2012-09-18 19:28:02 +0400

    Added Bayesian classification tutorial
    Change-Id: I66639893e9260bb160a8a2e35bec639e07f06b4d

---
+++ doc/examples/bayesian_classification_tutorial/bayesian_classification_tutorial_example1.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+#include "ltr/data/object.h"
+#include "ltr/data/object_pair.h"
+#include "ltr/data/object_list.h"
+#include "ltr/data/data_set.h"
+#include "ltr/density_estimators/base_probability_density_learner.h"
+#include "ltr/density_estimators/fisher_discriminant_density_learner.h"
+#include "ltr/density_estimators/fisher_discriminant.h"
+#include "ltr/learners/bayesian_learner/bayesian_learner.h"
+#include "ltr/scorers/bayesian_scorer.h"
+
+using std::cout;
+using std::endl;
+
+using ltr::Object;
+using ltr::DataSet;
+using ltr::BayesianScorer;
+using ltr::BayesianLearner;
+using ltr::FisherDiscriminant;
+using ltr::FisherDiscriminantLearner;
+using ltr::FisherDiscriminantDensityLearner;
+using ltr::BaseProbabilityDensityLearner;
+
+int main() {
+  Object object1;
+  object1 << 0 << 0;
+  object1.set_actual_label(1.0);
+
+  Object object2;
+  object2 << 10 << 0;
+  object2.set_actual_label(0.0);
+
+  Object object3;
+  object3 << 12 << 0;
+  object3.set_actual_label(0.0);
+
+  DataSet<Object> data;
+  data.add(object1);
+  data.add(object2);
+  data.add(object3);
+  FisherDiscriminantLearner<Object> learner;
+  learner.learn(data);
+  BayesianScorer::Ptr scorer = learner.makeSpecific();
+
+  Object object4;
+  object4 << 1 << 0;
+  cout << scorer->score(object4) << endl;
+}
+
