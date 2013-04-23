952c9e03f1b1930c4cfa95b67a8e84ba254a4aa6
arbabenko <arbabenko@yandex-team.ru>
2013-04-23 13:00:26 +0400

    hook fix

--- jenkins/build.sh
+++ jenkins/build.sh
@@ -3,11 +3,12 @@ set -e
 [ -z $JOBS ] && JOBS=2
 ! [ -d build ] && mkdir build
 rm -rf build/*
-cd build
 if [ "$OS" = "Windows_NT" ]; then
+  cd build
   $COMSPEC /c '..\jenkins\build.cmd'
 else
   python ./pre-push_check.py
+  cd build
   cmake ../
   make -j$JOBS RUN_SERIALIZATION
   make -j$JOBS
