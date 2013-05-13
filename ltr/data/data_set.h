7c2c478466278fbc334b9b0eed0561d836c501b7
wd28 <wd28@yandex-team.ru>
2013-05-14 02:59:19 +0400

    Added iterators over elements and objects to data set.
    Change-Id: I6bf4c81bf119ce23e4a2f63000fdafe69a749bdf

--- ltr/data/data_set.h
+++ ltr/data/data_set.h
@@ -20,13 +20,15 @@
 #include "ltr/data/per_object_accessor.h"
 #include "ltr/interfaces/aliaser.h"
 
-
-
 namespace ltr {
 /** 
  * The default weight for an element in data set.
  */
 const double DEFAULT_ELEMENT_WEIGHT = 1.0;
+/**
+ * The end element index of an iterator
+ */
+const int END_OBJECT_LINE_INDEX = -1;
 
 using std::vector;
 using std::map;
@@ -36,6 +38,18 @@ using ltr::utility::DoubleEqual;
 using Eigen::VectorXd;
 using Eigen::MatrixXd;
 
+template <typename TElement>
+class DataSetObjectIterator;
+
+template<typename TElement>
+class DataSetElementIterator;
+
+template<typename TElement>
+class ConstDataSetObjectIterator;
+
+template<typename TElement>
+class ConstDataSetElementIterator;
+
 /**
  * \brief Template class that implements DataSet.
  *
@@ -43,13 +57,21 @@ using Eigen::MatrixXd;
  * elements should implement Object container interface. Those could be
  * Object, ObjectPair or ObjectList.
  */
-template <typename TElement>
+template<typename TElement>
 class DataSet : public Printable, public Aliaser {
  public:
   /**
    * Shared pointer type to the DataSet.
    */
   typedef ltr::utility::shared_ptr<DataSet> Ptr;
+  /** Iterator over all objects in DataSet
+   */
+  typedef DataSetObjectIterator<TElement> object_iterator;
+  typedef ConstDataSetObjectIterator<TElement> const_object_iterator;
+  /** Iterator over elements of DataSet
+   */
+  typedef DataSetElementIterator<TElement> element_iterator;
+  typedef ConstDataSetElementIterator<TElement> const_element_iterator;
 
   /**
    * \param feature_info Information about features.
@@ -152,31 +174,71 @@ class DataSet : public Printable, public Aliaser {
    * Function for serialization dataset into string.
    */
   virtual string toString() const;
+  /**
+   * Start of non-constant iterator over objects in data set.
+   */
+  object_iterator object_begin();
+  /**
+   * End of non-constant iterator over objects in data set.
+   */
+  object_iterator object_end();
+  /**
+   * Start of constant iterator over objects in data set.
+   */
+  const_object_iterator object_begin() const;
+  /**
+   * End of constant iterator over objects in data set.
+   */
+  const_object_iterator object_end() const;
+  /**
+   * Start of non-constant iterator over elements in data set.
+   */
+  element_iterator element_begin();
+  /**
+   * End of non-constant iterator over elements in data set.
+   */
+  element_iterator element_end();
+  /**
+   * Start of constant iterator over elements in data set.
+   */
+  const_element_iterator element_begin() const;
+  /**
+   * End of constant iterator over elements in data set.
+   */
+  const_element_iterator element_end() const;
  private:
   /**
-   * Information about label value type
+   * Information about label value type.
    */
   ltr::utility::shared_ptr<LabelInfo> label_info_;
   /**
    * Shared pointer to the vector of elements' weights.
    */
   mutable vector<double> weights_;
-  /** Each element has its objects in the matrix ob lines, defined by this bounds
+  /**
+   * Each element has its objects in the matrix ob lines, defined by this bounds.
    */
   vector<ElementBounds> element_bounds_;
-  /** Representation for objects storage
+  /**
+   * Representation for objects storage.
    */
   shared_ptr<InnerRepresentation> inner_representation_;
   shared_ptr<FeatureInfo> feature_info_;
-  /** Vector of elements of the data set
+  /**
+   * Vector of elements of the data set.
    */
   vector< shared_ptr<TElement> > element_vector_;
   /**
-   * Returns default alias of class
+   * Returns default alias of class.
    */
   virtual string getDefaultAlias() const {
     return "DataSet";
   }
+
+  friend class DataSetObjectIterator<TElement>;
+  friend class ConstDataSetObjectIterator<TElement>;
+  friend class DataSetElementIterator<TElement>;
+  friend class ConstDataSetElementIterator<TElement>;
 };
 /**
  * Type for a data set that implements pointwise approach.
@@ -190,6 +252,238 @@ typedef DataSet<ObjectPair> PairwiseDataSet;
  * Type for a data set that implements listwise approach.
  */
 typedef DataSet<ObjectList> ListwiseDataSet;
+
+/**
+ * \brief Iterator over objects in data set.
+ */
+template<typename TElement>
+class DataSetObjectIterator {
+ public:
+  /**
+   * Copy constructor
+   */
+  DataSetObjectIterator(const DataSetObjectIterator& other):
+      data_set_(other.data_set_),
+      element_index_(other.element_index_),
+      current_object_(other.current_object_),
+      object_line_index_(other.object_line_index_) { }
+
+  /**
+   * Constructor that creates iterator on certain position.
+   *
+   * \param data_set Data set over which we iterate.
+   * \param element_number Number of starting element
+   * (Object, ObjectPair, ObjectList).
+   * \param current_object_line_index Index of starting object in data set. 
+   */
+  DataSetObjectIterator(DataSet<TElement>* data_set,
+                        int element_number,
+                        int current_object_line_index);
+
+  /**
+   * Iterator equality comparer.
+   */
+  bool operator==(const DataSetObjectIterator<TElement>& rhs) {
+    return this->data_set_ == rhs.data_set_ &&
+    this->object_line_index_ == rhs.object_line_index_ &&
+    this->element_index_ == rhs.element_index_;
+  }
+
+  bool operator!=(const DataSetObjectIterator& rhs) {
+    return !(this->operator==(rhs));
+  }
+
+  /**
+   * Gets current object. Throws std::logic_error if iterator is at its end.
+   */
+  Object& operator*();
+  /**
+   * Increments iterator. Returns reference to itself
+   */
+  DataSetObjectIterator& operator++();
+
+ private:
+  /**
+   * Generates object from given presentation and object line in it.
+   * \param ptr Pointer to data set representation.
+   * \param current_line Index of object to generate.
+   */
+  Object* generate_object(InnerRepresentation::Ptr ptr, int current_line);
+
+  DataSet<TElement>* data_set_;
+  int element_index_;
+  int object_line_index_;
+
+  shared_ptr<Object> current_object_;
+};
+
+/**
+ * \brief Constant iterator over objects in data set.
+ */
+template<typename TElement>
+class ConstDataSetObjectIterator {
+ public:
+  /**
+   * Copy constructor
+   */
+  ConstDataSetObjectIterator(const ConstDataSetObjectIterator& other):
+    data_set_(other.data_set_),
+    element_index_(other.element_index_),
+    current_object_(other.current_object_),
+    object_line_index_(other.object_line_index_) { }
+
+  /**
+   * Constructor that creates iterator on certain position.
+   *
+   * \param data_set Data set over which we iterate.
+   * \param element_number Number of starting element
+   * (Object, ObjectPair, ObjectList).
+   * \param current_object_line_index Index of starting object in data set. 
+   */
+  ConstDataSetObjectIterator(const DataSet<TElement>* data_set,
+                       int element_number, int current_object_line_index);
+
+  /**
+   * Iterator equality comparer.
+   */
+  bool operator==(const ConstDataSetObjectIterator<TElement>& rhs) {
+    return this->data_set_ == rhs.data_set_ &&
+    this->object_line_index_ == rhs.object_line_index_ &&
+    this->element_index_ == rhs.element_index_;
+  }
+
+  bool operator!=(const ConstDataSetObjectIterator& rhs) {
+    return !(this->operator==(rhs));
+  }
+
+  /**
+   * Gets current object. Throws std::logic_error if iterator is at its end.
+   */
+  const Object& operator*();
+
+  /**
+   * Increments iterator. Returns reference to itself
+   */
+  ConstDataSetObjectIterator& operator++();
+
+ private:
+  /**
+   * Generates object from given presentation and object line in it.
+   * \param ptr Pointer to data set representation.
+   * \param current_line Index of object to generate.
+   */
+  Object* generate_object(InnerRepresentation::Ptr ptr, int current_line);
+
+  const DataSet<TElement>* data_set_;
+  int element_index_;
+  int object_line_index_;
+
+  shared_ptr<Object> current_object_;
+};
+
+
+/**
+ * \brief Iterator over elements in data set.
+ */
+template<typename TElement>
+class DataSetElementIterator {
+ public:
+  /**
+   * Copy constructor
+   */
+  DataSetElementIterator(const DataSetElementIterator& other):
+      data_set_(other.data_set_),
+      element_index_(other.element_index_)
+      { }
+
+  /**
+   * Constructor that creates iterator on certain position.
+   *
+   * \param data_set Data set over which we iterate.
+   * \param element_number Number of starting element
+   * (Object, ObjectPair, ObjectList).
+   */
+  DataSetElementIterator(DataSet<TElement>* data_set,
+                         int element_number);
+
+  /**
+   * Iterator equality comparer.
+   */
+  bool operator==(const DataSetElementIterator<TElement>& rhs) {
+    return this->data_set_ == rhs.data_set_ &&
+    this->element_index_ == rhs.element_index_;
+  }
+
+  bool operator!=(const DataSetElementIterator& rhs) {
+    return !(this->operator==(rhs));
+  }
+
+  /**
+   * Gets current element (Object, ObjectPair, ObjectList).
+   * Throws std::logic_error if iterator is at its end.
+   */
+  TElement& operator*();
+
+  /**
+   * Increments iterator. Returns reference to itself
+   */
+  DataSetElementIterator& operator++();
+
+ private:
+  DataSet<TElement>* data_set_;
+  int element_index_;
+};
+
+/**
+ * \brief Constant iterator over elements in data set.
+ */
+template<typename TElement>
+class ConstDataSetElementIterator {
+ public:
+  /**
+   * Copy constructor
+   */
+  ConstDataSetElementIterator(const ConstDataSetElementIterator& other):
+      data_set_(other.data_set_),
+      element_index_(other.element_index_)
+      { }
+
+  /**
+   * Constructor that creates iterator on certain position.
+   *
+   * \param data_set Data set over which we iterate.
+   * \param element_number Number of starting element
+   * (Object, ObjectPair, ObjectList).
+   */
+  ConstDataSetElementIterator(const DataSet<TElement>* data_set,
+                              int element_number);
+
+  /**
+   * Iterator equality comparer.
+   */
+  bool operator==(const ConstDataSetElementIterator<TElement>& rhs) {
+    return this->data_set_ == rhs.data_set_ &&
+    this->element_index_ == rhs.element_index_;
+  }
+
+  bool operator!=(const ConstDataSetElementIterator& rhs) {
+    return !(this->operator==(rhs));
+  }
+
+  /**
+   * Gets current object. Throws std::logic_error if iterator is at its end.
+   */
+  const TElement& operator*();
+
+  /**
+   * Increments iterator. Returns reference to itself
+   */
+  ConstDataSetElementIterator& operator++();
+
+ private:
+  const DataSet<TElement>* data_set_;
+  int element_index_;
+};
 
 template <typename TElement>
 DataSet<TElement>::DataSet(const FeatureInfo& feature_info,
@@ -350,7 +644,9 @@ DataSet<TElement>::lightSubset(const vector<int>& indices) const {
   DataSet result(feature_info());
   result.inner_representation_ = this->inner_representation_;
 
-  for (int indices_index = 0; indices_index < (int)indices.size(); ++indices_index) {  // NOLINT
+  for (int indices_index = 0;
+       indices_index < (int)indices.size();
+       ++indices_index) {
     CHECK(indices[indices_index] < size() && indices[indices_index] >= 0);
     result.element_bounds_.push_back(element_bounds_[indices[indices_index]]);
     result.element_vector_.push_back(
@@ -368,6 +664,193 @@ inline string DataSet<TElement>::toString() const {
     str << (*this)[element_index].toString() << std::endl;
   }
   return str.str();
+}
+
+template <typename TElement>
+typename DataSet<TElement>::object_iterator DataSet<TElement>::object_begin() {
+  if (size() == 0) {
+    return object_iterator(this, 0, END_OBJECT_LINE_INDEX);
+  } else {
+    return object_iterator(this, 0, element_bounds_[0].start_line_index);
+  }
+}
+
+template <typename TElement>
+typename DataSet<TElement>::object_iterator DataSet<TElement>::object_end() {
+  return object_iterator(this, size(), END_OBJECT_LINE_INDEX);
+}
+
+template <typename TElement>
+inline typename DataSet<TElement>::const_object_iterator
+  DataSet<TElement>::object_begin() const {
+  if (size() == 0) {
+    return const_object_iterator(this, 0, END_OBJECT_LINE_INDEX);
+  } else {
+    return const_object_iterator(this, 0,
+                                 element_bounds_[0].start_line_index);
+  }
+}
+
+template <typename TElement>
+inline typename DataSet<TElement>::const_object_iterator
+  DataSet<TElement>::object_end() const {
+  return const_object_iterator(this, size(), END_OBJECT_LINE_INDEX);
+}
+
+template <typename TElement>
+typename DataSet<TElement>::element_iterator
+  DataSet<TElement>::element_begin() {
+    return element_iterator(this, 0);
+}
+
+template <typename TElement>
+typename DataSet<TElement>::element_iterator
+  DataSet<TElement>::element_end() {
+    return element_iterator(this, size());
+}
+
+template <typename TElement>
+typename DataSet<TElement>::const_element_iterator
+  DataSet<TElement>::element_begin() const {
+    return const_element_iterator(this, 0);
+}
+
+template <typename TElement>
+typename DataSet<TElement>::const_element_iterator
+  DataSet<TElement>::element_end() const {
+    return const_element_iterator(this, size());
+}
+
+template <typename TElement>
+DataSetObjectIterator<TElement>::DataSetObjectIterator(
+    DataSet<TElement>* data_set,
+    int element_number,
+    int current_object_line_index): data_set_(data_set),
+                              element_index_(element_number),
+                              object_line_index_(current_object_line_index) {
+    current_object_ = generate_object(data_set_->inner_representation_,
+                                      object_line_index_);
+}
+
+template <typename TElement>
+ConstDataSetObjectIterator<TElement>::ConstDataSetObjectIterator(
+    const DataSet<TElement>* data_set,
+    int element_number,
+    int current_object_line_index): data_set_(data_set),
+                              element_index_(element_number),
+                              object_line_index_(current_object_line_index) {
+    current_object_ = generate_object(data_set_->inner_representation_,
+                                      object_line_index_);
+}
+
+template <typename TElement>
+DataSetElementIterator<TElement>::DataSetElementIterator(
+    DataSet<TElement>* data_set,
+    int element_number): data_set_(data_set),
+                         element_index_(element_number) { }
+
+template <typename TElement>
+ConstDataSetElementIterator<TElement>::ConstDataSetElementIterator(
+    const DataSet<TElement>* data_set,
+    int element_number): data_set_(data_set),
+                         element_index_(element_number) { }
+
+template <typename TElement>
+Object& DataSetObjectIterator<TElement>::operator*() {
+  if (current_object_.get() == NULL)
+    throw std::logic_error("Iterator is out of range");
+  return *current_object_;
+}
+
+template <typename TElement>
+const Object& ConstDataSetObjectIterator<TElement>::operator*() {
+  if (current_object_.get() == NULL)
+    throw std::logic_error("Iterator is out of range");
+  return *current_object_;
+}
+
+template <typename TElement>
+TElement& DataSetElementIterator<TElement>::operator*() {
+  return (*data_set_)[element_index_];
+}
+
+template <typename TElement>
+const TElement& ConstDataSetElementIterator<TElement>::operator*() {
+  return (*data_set_)[element_index_];
+}
+
+template <typename TElement>
+DataSetObjectIterator<TElement>&
+DataSetObjectIterator<TElement>::operator++() {
+  ++object_line_index_;
+  int last_index = data_set_->element_bounds_[element_index_].end_line_index;
+  if (object_line_index_ == last_index) {
+    ++element_index_;
+    if (element_index_ == data_set_->size()) {
+      object_line_index_ = END_OBJECT_LINE_INDEX;
+    } else {
+      object_line_index_ =
+        data_set_->element_bounds_[element_index_].start_line_index;
+    }
+  }
+  current_object_ = generate_object(data_set_->inner_representation_,
+                                    object_line_index_);
+  return *this;
+}
+
+template <typename TElement>
+ConstDataSetObjectIterator<TElement>&
+ConstDataSetObjectIterator<TElement>::operator++() {
+  ++object_line_index_;
+  int last_index = data_set_->element_bounds_[element_index_].end_line_index;
+  if (object_line_index_ == last_index) {
+    ++element_index_;
+    if (element_index_ == data_set_->size()) {
+      object_line_index_ = END_OBJECT_LINE_INDEX;
+    } else {
+      object_line_index_ =
+        data_set_->element_bounds_[element_index_].start_line_index;
+    }
+  }
+  current_object_ = generate_object(data_set_->inner_representation_,
+                                    object_line_index_);
+  return *this;
+}
+
+template <typename TElement>
+DataSetElementIterator<TElement>&
+DataSetElementIterator<TElement>::operator++() {
+  ++element_index_;
+  return *this;
+}
+
+template <typename TElement>
+ConstDataSetElementIterator<TElement>&
+ConstDataSetElementIterator<TElement>::operator++() {
+  ++element_index_;
+  return *this;
+}
+
+template<typename TElement>
+Object* DataSetObjectIterator<TElement>::generate_object(
+  InnerRepresentation::Ptr ptr,
+  int current_line) {
+    if (current_line == END_OBJECT_LINE_INDEX) {
+      return NULL;
+    } else {
+      return new Object(ptr, current_line);
+    }
+}
+
+template<typename TElement>
+Object* ConstDataSetObjectIterator<TElement>::generate_object(
+  InnerRepresentation::Ptr ptr,
+  int current_line) {
+    if (current_line == END_OBJECT_LINE_INDEX) {
+      return NULL;
+    } else {
+      return new Object(ptr, current_line);
+    }
 }
 
 template<typename TElement>
