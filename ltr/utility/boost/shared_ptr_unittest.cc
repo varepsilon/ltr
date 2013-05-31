0164cf8e2cbaab09c6e9669e3a4d04e95b7f0dc0
skyhawk <skyhawk@yandex-team.ru>
2013-06-01 00:42:38 +0400

    Bug in shared_ptr fixed.
    Change-Id: Ia3186e490ca7a0c5ed3cf8bd636311beac2dd588

--- ltr/utility/boost/shared_ptr_unittest.cc
+++ ltr/utility/boost/shared_ptr_unittest.cc
@@ -11,14 +11,19 @@ TEST(SharedPtrTest, SharedPtrTest) {
   shared_ptr<int> p1 = p0;
   EXPECT_EQ(p0, p1);
 
-  shared_ptr<int> p2(new int(42));
+  shared_ptr<int> p2(new int(42)); // NOLINT
   shared_ptr<int> p3 = p2;
   EXPECT_EQ(*p2, 42);
   EXPECT_EQ(*p3, 42);
 
-  p3.reset(new int(1));
+  p3.reset(new int(1)); // NOLINT
   EXPECT_EQ(*p3, 1);
 
   p0.swap(p2);
   EXPECT_EQ(*p0, 42);
+
+  shared_ptr<int> p4 = NULL;
+  shared_ptr<int> p5 = new int(42); // NOLINT
+  EXPECT_TRUE(p5);
+  EXPECT_FALSE(p4);
 }
