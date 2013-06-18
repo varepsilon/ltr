e5960349b9b2ba31527c5d21614eb334ec7e3bf0
skyhawk <skyhawk@yandex-team.ru>
2013-06-18 12:48:17 +0400

    Documentation checking script fixed
    Change-Id: I6bcdbee9551ee2af606e4ab4340a017f46c30550

--- jenkins/doxygen.sh
+++ jenkins/doxygen.sh
@@ -1,7 +1,28 @@
 # Script for check documentation
-cd ..;
-if ( doxygen doc/doxygen.cfg 2>&1 1>$- | grep -w '..*'; ); then
-  echo "[ERROR] Documentation generation failed!"
-  false
+if ! which doxygen >/dev/null; then
+  echo "[WARNING] Doxygen not installed. Documentation checking skipped"
+  exit 0
 fi
-cd jenkins;
+
+DOX_VERSION=`doxygen --version`
+if [ $DOX_VERSION \< 1.8.0 ]; then
+  echo -n "[WARNING] Doxygen version is too old. "
+  echo "Need: 1.8.0. Have: " $DOX_VERSION
+  echo "Documentation checking skipped"
+  exit 0
+fi
+
+
+echo "[DOXYGEN] Start"
+cd ..
+DOX_OUTPUT=`doxygen doc/doxygen.cfg 2>&1 1>/dev/null  | sort -u`
+cd jenkins
+
+if [ `echo $DOX_OUTPUT | wc -c` -gt 1 ]; then
+  echo "$DOX_OUTPUT"
+  echo "[ERROR] Documentation generation failed!"
+  exit 1
+fi
+
+echo "[DOXYGEN] Success"
+exit 0
